e966fa45cfa6856e7b041db9b9f3d834a452257a
shivajiveera <shivajiveera12@gmail.com>
2023-02-01 00:16:27 +0530

    adding the file in my directory

---
+++ src/data/ProjectData.js
@@ -0,0 +1,45 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://raw.githubusercontent.com/shivajiveera/aeroplane-rental/main/Screenshot%20(8).png",
+    title: "Air Service Rental",
+    description: "Air Service Rental” Angular JS | CSS | HTML | Bootstrap • Full Air Service Rental Website with Book Now, Form To accept Travel Details.",
+    tech_stack: "Angular, css, Bootstrap, Html ",
+    github_url: "https://github.com/shivajiveera/aeroplane-rental",
+    demo_url: "https://github.com/shivajiveera/aeroplane-rental",
+  },
+ 
+];
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    name: "ReactJS",
+  },
+
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+  {
+    img: "https://angular.io/assets/images/logos/angular/angular.svg",
+    name: "Responsive",
+  },
+];
